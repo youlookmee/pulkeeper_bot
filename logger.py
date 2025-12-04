61f2cac5a73da32a8468f45af6341be0117e1616
youlookmee <uraloffsarik@gmail.com>
2025-12-04 17:56:58 +0500

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,16 @@
+import logging
+
+def get_logger(name: str = "finbot"):
+    """
+    Возвращает настроенный logger.
+    """
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
