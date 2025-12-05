79f0f7cb2686e06da6c06be330571f0f01b7eeb5
youlookmee <uraloffsarik@gmail.com>
2025-12-05 15:07:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from config import TELEGRAM_TOKEN
 
 # Handlers
 from handlers import transaction_handler
-from handlers.report_handler import report_handler
-from handlers.month_handler import month_handler
+from handlers import report_handler
+from handlers import month_handler
 
 # DB
 from services.db import init_db
