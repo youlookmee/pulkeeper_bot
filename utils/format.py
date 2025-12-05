43944b2add282185476da62bb5ac00dcacdebeaa
youlookmee <uraloffsarik@gmail.com>
2025-12-05 16:07:20 +0500

    Update format.py

--- utils/format.py
+++ utils/format.py
@@ -1,2 +1,12 @@
-def fmt(amount: float) -> str:
+def money_format(amount: float) -> str:
+    """
+    Форматирование суммы:
+    150000 → 150,000
+    20000 → 20,000
+    """
+    try:
+        amount = float(amount)
+    except:
+        return str(amount)
+
     return f"{amount:,.0f}".replace(",", " ")
