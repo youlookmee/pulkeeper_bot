434e9695be372542ebf2ae6cfd5380d4f80e0b6a
youlookmee <uraloffsarik@gmail.com>
2025-12-05 16:10:09 +0500

    Update format.py

--- utils/format.py
+++ utils/format.py
@@ -1,12 +1,31 @@
 def money_format(amount: float) -> str:
     """
     Форматирование суммы:
-    150000 → 150,000
-    20000 → 20,000
+    15000 → 15 тыс.
+    150000 → 150 тыс.
+    1500000 → 1.5 млн
+    20000000 → 20 млн
     """
     try:
         amount = float(amount)
     except:
         return str(amount)
 
+    # млн
+    if amount >= 1_000_000:
+        formatted = amount / 1_000_000
+        if formatted.is_integer():
+            return f"{int(formatted)} млн"
+        else:
+            return f"{formatted:.1f} млн"
+
+    # тыс.
+    if amount >= 10_000:
+        formatted = amount / 1000
+        if formatted.is_integer():
+            return f"{int(formatted)} тыс."
+        else:
+            return f"{formatted:.1f} тыс."
+
+    # меньше 10k — выводим как есть
     return f"{amount:,.0f}".replace(",", " ")
